16dc246b932a6234f62b529f92476cd43a7992c0
HARSH <harshsennn@gmail.com>
2024-11-17 23:52:01 +0530

    Added Cpu usage

--- Backend/main.go
+++ Backend/main.go
@@ -10,3 +10,14 @@ import (
     "github.com/shirou/gopsutil/v3/net"
     "github.com/rs/cors"
 )
+
+func getSystemMetrics(w http.ResponseWriter, r *http.Request) {
+	/*For CPU Usage*/
+	cpuPercent, err := cpu.Percent(0, false)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to fetch CPU data: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+// Memory Usage
