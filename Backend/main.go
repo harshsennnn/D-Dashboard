60249074c59e015c26c3c137cbb1218d999ee01d
HARSH <harshsennn@gmail.com>
2024-11-17 18:48:39 +0530

    add packages

--- Backend/main.go
+++ Backend/main.go
@@ -1,3 +1,12 @@
 package main
-
-import ()
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+    "github.com/shirou/gopsutil/v3/cpu"
+    "github.com/shirou/gopsutil/v3/mem"
+    "github.com/shirou/gopsutil/v3/disk"
+    "github.com/shirou/gopsutil/v3/net"
+    "github.com/rs/cors"
+)
